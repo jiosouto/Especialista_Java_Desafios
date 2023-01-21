11ccd7efce0e9b9efebc8ece980862176e3b8458
Jefferson Souto <inayan@outlook.com>
2023-01-20 21:49:22 -0300

    Wrappers e boxing

--- 02. Mergulhando em Orientação a Objetos/Desafio_13_Wrappers_Boxing/Televisor.java
+++ 02. Mergulhando em Orientação a Objetos/Desafio_13_Wrappers_Boxing/Televisor.java
@@ -1,23 +1,29 @@
+import java.util.Objects;
+
 public class Televisor {
 
     Integer canal = 130;
     Integer volume = 20;
 
     void mudarCanal(Integer novoCanal) {
-        if (canal == novoCanal) {
+        Objects.requireNonNull(novoCanal, "Novo canal deve ser informado");
+
+        if (canal.equals(novoCanal)) {
             System.out.println("Novo canal é também o canal atual.");
         } else {
             canal = novoCanal;
-            System.out.println("Canal alterado para " + canal);
+            System.out.printf("Canal alterado para %d%n", canal);
         }
     }
 
     void mudarVolume(Integer novoVolume) {
-        if (novoVolume == volume) {
+        Objects.requireNonNull(novoVolume, "Novo volume deve ser informado");
+
+        if (volume.equals(novoVolume)) {
             System.out.println("Novo volume é também o volume atual.");
         } else {
-            volume = Integer.valueOf(novoVolume.byteValue());
-            System.out.println("Volume alterado para " + volume);
+            volume = novoVolume;
+            System.out.printf("Volume alterado para %d%n", volume);
         }
     }
 
