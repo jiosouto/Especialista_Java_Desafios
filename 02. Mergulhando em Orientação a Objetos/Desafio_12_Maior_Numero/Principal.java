8918130cfa796e0392e51c5bf431910a0694d239
Jefferson Souto <inayan@outlook.com>
2023-01-20 21:31:26 -0300

    Wrappers e boxing

--- 02. Mergulhando em Orientação a Objetos/Desafio_12_Maior_Numero/Principal.java
+++ 02. Mergulhando em Orientação a Objetos/Desafio_12_Maior_Numero/Principal.java
@@ -1,11 +1,15 @@
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println(NumeroUtil.descobrirMaior(10, 9));
-        System.out.println(NumeroUtil.descobrirMaior(25, 21, 22));
+        Televisor tv = new Televisor();
 
-        System.out.println(NumeroUtil.descobrirMaior(2.9, 1.8));
-        System.out.println(NumeroUtil.descobrirMaior(30.0, 30.1, 20.4));
+        // Não deveria mudar o volume e canal
+        tv.mudarVolume(20);
+        tv.mudarCanal(130);
+
+        // Deveria mudar o volume e canal
+        tv.mudarVolume(300);
+        tv.mudarCanal(10);
     }
 
 }
