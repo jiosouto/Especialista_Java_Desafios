cf4b7a37c85b9f222b98f9e31f361106140bc626
Jefferson Souto <inayan@outlook.com>
2023-01-21 23:17:54 -0300

    Arrays

--- 04. Trabalhando com Arrays/Desafio_15_Cardapio/Cardapio.java
+++ 04. Trabalhando com Arrays/Desafio_15_Cardapio/Cardapio.java
@@ -1,18 +1,30 @@
+import java.util.Arrays;
+
 public class Cardapio {
 
-    ItemCardapio[] itens;
+    ItemCardapio[] itens = new ItemCardapio[0];
 
     void adicionarItem(ItemCardapio item) {
-        // TODO implementar inclusão de item do cardápio
+        itens = Arrays.copyOf(itens, itens.length + 1);
+        itens[itens.length - 1] = item;
     }
 
     void removerItem(int indice) {
-        // TODO implementar exclusão de item do cardápio da posição informada
+        ItemCardapio[] novosItens = new ItemCardapio[itens.length - 1];
+
+        System.arraycopy(itens, 0, novosItens, 0, indice);
+        System.arraycopy(itens, indice + 1,
+                novosItens, indice, novosItens.length - indice);
+
+        itens = novosItens;
     }
 
     void imprimirItensCardapio(double precoMinimo, double precoMaximo) {
-        // TODO implementar código para imprimir na console os itens
-        //  do cardápio que estiverem entre o preço mínimo e máximo
+        for (ItemCardapio item : itens) {
+            if (item.possuiPrecoEntre(precoMinimo, precoMaximo)) {
+                item.imprimir();
+            }
+        }
     }
 
 }
